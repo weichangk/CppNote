56f8d8edac5c6765df362b3d14d827b1a4c578e8
weick <weick@300624.cn>
2023-03-03 18:02:25 +0800

    add

--- vscodecmakecpp/src/main.cpp
+++ vscodecmakecpp/src/main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
+#include "test.h"
 using namespace std;
 
-void swap(int &a, int &b)
-{
-  int temp = a;
-  a = b;
-  b = temp;
-}
 
 int main(int argc, char const *argv[])
 {
+  cout << "hello World" << endl;
+
+  testa = 100;
+  cout << "testa = "<< testa << endl;
+
   int v1 = 10;
   int v2 = 20;
   cout << "before swap:" << endl;
   cout << "v1 = "<< v1 << endl;
   cout << "v2 = "<< v2 << endl;
-  swap(v1, v2);
+  Test t;
+  t.swap(v1, v2);
   cout << "after swap:" << endl;
   cout << "v1 = "<< v1 << endl;
   cout << "v2 = "<< v2 << endl;
+
+
   return 0;
 }
