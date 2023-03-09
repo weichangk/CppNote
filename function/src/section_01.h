00a0f8a32b347bb779477777fc694114865916d6
weick <weick@300624.cn>
2023-03-09 19:51:59 +0800

    add

---
+++ function/src/section_01.h
@@ -0,0 +1,14 @@
+#pragma once
+
+void changeValue(int n);
+void func01();
+void changeValueByRef(int& n);
+void func02();
+void changeValueByPtr(int* n);
+void func03();
+void printArray(int arr[], int size);
+void func04();
+void printArray1(int* arr, int size);
+void func05();
+void printArray(int (&arr)[5]);
+void func06();
