e789097b66202affd901275ba0fe62786d6dcb07
weick <weick@300624.cn>
2023-03-09 20:35:03 +0800

    add

--- function/src/main.cpp
+++ function/src/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include "section_01.h"
+#include "section_02.h"
 using namespace std;
 
 void section_01()
 {
-  func01();
-  func02();
-  func03();
-  func04();
-  func05();
-  func06();
+  cout << "函数原型、函数签名、函数定义、函数参数传递方式" << endl;
+  ns_section_01::func01();
+  ns_section_01::func02();
+  ns_section_01::func03();
+}
+void section_02()
+{
+  cout << "函数参数-数组、结构体、字符串、array对象" << endl;
+  ns_section_02::func01();
+  ns_section_02::func02();
+  ns_section_02::func03();
 }
 int main(int argc, char const *argv[])
 {
   cout << "hello function" << endl;
   section_01();
+  section_02();
 }
---
+++ function/src/section_02.h
@@ -0,0 +1,11 @@
+#pragma once
+namespace ns_section_02
+{
+  void printArray(int arr[], int size);
+  void func01();
+  void printArray1(int* arr, int size);
+  void func02();
+  void printArray(int (&arr)[5]);
+  void func03();
+}
+
